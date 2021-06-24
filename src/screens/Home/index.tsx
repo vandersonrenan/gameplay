5e34f4346d03f5843a69bf7b7d28b3b9b1a97614
Vanderson Rocha <vandersonrocha@Vandersons-MacBook-Pro.local>
2021-06-23 22:12:37 -0300

    third commit

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { View, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { CategorySelect } from '../../components/CategorySelect';
 import { Appointment } from '../../components/Appointment';
 import { ListHeader } from '../../components/ListHeader';
+import { Background } from '../../components/Background'
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { Profile } from '../../components/Profile';
 import { ListDivider } from '../../components/ListDivider';
@@ -12,6 +14,8 @@ import { styles } from './styles';
  
 export function Home() {
   const [category, setCategory] = useState('');
+
+  const navigation = useNavigation();
 
   const appointments = [
     {
@@ -44,11 +48,19 @@ export function Home() {
     categoryId === category ? setCategory('') : setCategory(categoryId);
   }
 
+  function handleAppointmentDetails(){
+    navigation.navigate('AppointmentDetails')
+  }
+  
+  function handleAppointmentCreate(){
+    navigation.navigate('AppointmentCreate')
+  }
+
   return (
-    <View>
+    <Background>
       <View style={styles.header}>
         <Profile />
-        <ButtonAdd />
+        <ButtonAdd onPress={handleAppointmentCreate}/>
       </View>
 
       <CategorySelect 
@@ -66,13 +78,16 @@ export function Home() {
           data={appointments}
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
-          <Appointment data={item} />
+          <Appointment 
+          data={item} 
+          onPress={handleAppointmentDetails}
+          />
           )}
           ItemSeparatorComponent={() => <ListDivider />}
           style={styles.matches}
           showsVerticalScrollIndicator={false}
         />
       </View>
-    </View>
+    </Background>
   );
 }
