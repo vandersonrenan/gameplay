427c69285767e5d812407df065c053ff34fa4919
Vanderson Rocha <vandersonrocha@angnb00029.angeloni.com.br>
2021-06-25 09:09:59 -0300

    fourth commit

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -68,13 +68,12 @@ export function Home() {
         setCategory={handleCategorySelect}
       />
 
-      <View style={styles.content}>
-        <ListHeader 
-          title="Partidas Agendadas"
-          subtitle="Total 6"
-        />
-        
-        <FlatList 
+      <ListHeader 
+        title="Partidas Agendadas"
+        subtitle="Total 6"
+      />
+
+      <FlatList 
           data={appointments}
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
@@ -84,10 +83,10 @@ export function Home() {
           />
           )}
           ItemSeparatorComponent={() => <ListDivider />}
+          contentContainerStyle={{ paddingBottom: 27 }}
           style={styles.matches}
           showsVerticalScrollIndicator={false}
         />
-      </View>
     </Background>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,21 +2,29 @@ import React from 'react';
 import { 
   View, 
   Text, 
-  Image
+  Image,
+  Alert,
+  ActivityIndicator
 } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+
+import { useAuth } from '../../hooks/auth';
 
 import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles' 
+import { theme } from '../../global/styles/theme';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { Background } from '../../components/Background'
 
 export function SignIn(){
-  const navigation = useNavigation()
+  const { loading, signIn } = useAuth();
 
-  function handleSignIn() {
-    navigation.navigate('Home');
+  async function handleSignIn() {
+    try {
+      await signIn()
+    } catch (error) {
+      Alert.alert(error);
+    }
   }
 
   return(
@@ -40,10 +48,14 @@ export function SignIn(){
             favoritos com seus amigos
           </Text>
 
-          <ButtonIcon 
-          title="Entrar com Discord"
-          onPress={handleSignIn}
-          />
+          {
+            loading ? <ActivityIndicator color={theme.colors.primary}/>
+            :
+              <ButtonIcon 
+            title="Entrar com Discord"
+            onPress={handleSignIn}
+            />
+          }
 
         </View>
     </View>
